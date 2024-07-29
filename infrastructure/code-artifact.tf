6cb295276954f2a85ebc8f69be72862c2209d313
Lukas Hartrumpf <lukas.hartrumpf@nerdware.dev>
2024-07-29 09:25:41 +0200

    feat: remove code artifact

--- infrastructure/code-artifact.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_codeartifact_domain" "this" {
-  domain = "semantic-release-domain"
-}
-
-resource "aws_codeartifact_repository" "this" {
-  repository = "semantic-release-repo"
-  domain     = aws_codeartifact_domain.this.domain
-}
